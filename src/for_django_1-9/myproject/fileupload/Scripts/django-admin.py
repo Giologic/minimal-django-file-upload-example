333d900d54d13c2c6327b1b9cc2ad8f228f304c6
Giologic <gio_velez@dlsu.edu.ph>
2017-06-17 13:21:26 +0800

    Tested Functionlity

---
+++ src/for_django_1-9/myproject/fileupload/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\gio\git\minimal-django-file-upload-example\src\for_django_1-9\myproject\fileupload\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
